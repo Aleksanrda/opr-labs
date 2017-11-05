7c750a8251a4c36dbddd3f1c04a064696bc4a009
Sasha <oleksandra.kryvko@nure.ua>
2017-11-05 13:56:53 +0200

    Refactoring of lab1.2

--- opr.laba1.2.js
+++ opr.laba1.2.js
@@ -1,18 +1,20 @@
 debugger;
 
 function inputNumber() {
-    number = parseInt(prompt('Enter your number:'))
-    return (number);
+    let number = parseInt(prompt('Enter your number:'))
+    return number;
 }
-
 
 function calculateSumOfPows() {
     let xsum = 0;
     let coef = 1;
+
     for (let numeric = 1; numeric <= number; numeric++) {
         xsum += coef * Math.pow(numeric, numeric);
         coef *= -1;
     }
-    return (xsum);
+
+    return xsum;
 }
+
 alert(calculateSumOfPows(inputNumber()));
