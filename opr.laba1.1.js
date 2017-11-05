e521c3dfe4cea66414db91c8cc90545bcb74ccee
Kostya Vyrodov <kostya.vyrodov@gmail.com>
2017-11-05 13:33:28 +0200

    Add function to split number

--- opr.laba1.1.js
+++ opr.laba1.1.js
@@ -1,47 +1,32 @@
-debugger;
-function inputNumbers() {
-    let numbers = [];
-    for (let i = 0; i < 8; i++) {
-        numbers[i] = parseInt(prompt('Enter your number:'));
-    }
-    return (numbers);
+var x = 123;
+x.toString()
+var res = [];
+var i = 0;
+
+while(x > 0){
+    var number = x % 10
+    var x = parseInt(x / 10);
+    res[i] = number
+    i++
 }
 
-function positiveCountNumber() {
-    let positiveCountNumber = 0;
-    for (let j = 0; j < numbers.length; j++) {
-        if (numbers[j] >= 0) {
-            positiveCountNumber++;
-        }
-    }
-}
+console.log(res);
 
+// function inputNumbers() {
+//     let numbers = [];
+//     for (let i = 0; i < 8; i++) {
+//         numbers[i] = parseInt(prompt('Enter your number:'));
+//     }
+//     return (numbers);
+// }
 
+// function positiveCountNumber() {
+//     let positiveCountNumber = 0;
+//     for (let j = 0; j < numbers.length; j++) {
+//         if (numbers[j] >= 0) {
+//             positiveCountNumber++;
+//         }
+//     }
+// }
 
-console.log(positiveCountNumber(inputNumbers()));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// console.log(positiveCountNumber(inputNumbers()));
